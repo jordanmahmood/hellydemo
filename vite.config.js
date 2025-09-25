657e6b555a631e93a9c9bc02f415614a4122a2a4
Jordan Mahmood <ymahmood@bu.edu>
2025-09-25 18:54:21 -0400

    vite.config.js fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    optimizeDeps: {
+        include: ['react', 'react-dom'],
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, './index.html'),
+            },
     server: {
         port: 3000,
         open: true,
         watch: {
             usePolling: true,
         },
-    },
+                },
+            },
+        },
+
 });
