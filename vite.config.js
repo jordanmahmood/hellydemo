9468dcb0fda890fd99a53d509fce56eab44d3a70
Jordan Mahmood <ymahmood@bu.edu>
2025-09-25 20:39:29 -0400

    remove vite

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        watch: {
-            usePolling: true,
-        },
-    },
     optimizeDeps: {
         include: ['react', 'react-dom'],
     },
